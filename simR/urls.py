3dd213ef7ec55ce2add62e988f210f9e84cf1b3c
PIA-Noah <noah.balencourt@parisinfrastructureadvisory.com>
2024-07-02 11:04:23 +0200

    changing accounts url redirection

--- simR/urls.py
+++ simR/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('allauth.urls')),
     path('', include('data.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
