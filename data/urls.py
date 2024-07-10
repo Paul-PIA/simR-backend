66578c9b0160c4789e2355bbf5120015a7390a9a
PIA-Noah <noah.balencourt@parisinfrastructureadvisory.com>
2024-07-10 17:06:57 +0200

    correction 6

--- data/urls.py
+++ data/urls.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
 from . import views
-
-
-from . import viewspip 
 from .routers import router
 
 app_name = "data"
