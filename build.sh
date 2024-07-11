dbeffea5772734dad4b3b5f0d0b4b23858255bbc
PIA-Noah <noah.balencourt@parisinfrastructureadvisory.com>
2024-07-11 11:01:33 +0200

    correcting the line 3

--- build.sh
+++ build.sh
@@ -10,4 +10,5 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate data zero
+python manage.py makemigrations --merge --no-input
 python manage.py migrate
