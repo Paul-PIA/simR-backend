5b150a8fff8771f230d281356812a79288fedfe0
PIA-Noah <noah.balencourt@parisinfrastructureadvisory.com>
2024-07-23 17:02:19 +0200

    deployment with static_root

--- simR/settings.py
+++ simR/settings.py
@@ -208,6 +208,8 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
