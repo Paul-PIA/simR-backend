c914d1f3655bde52b72a5de7e4593cbf51613845
PIA-Noah <noah.balencourt@parisinfrastructureadvisory.com>
2024-07-24 10:55:27 +0200

    deployment with updated database settings

--- simR/settings.py
+++ simR/settings.py
@@ -210,6 +210,8 @@ if not DEBUG:
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
