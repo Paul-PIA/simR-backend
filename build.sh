cbe15ba89f58258990b7dca70db1ae6a9f9fb676
PIA-Noah <noah.balencourt@parisinfrastructureadvisory.com>
2024-08-01 17:09:19 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,15 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Collect static files
+# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Force apply migrations
-python manage.py migrate --noinput
+# Apply any outstanding database migrations
 
+python manage.py makemigrations
+python manage.py makemigrations data
+python manage.py migrate
+python manage.py migrate data
