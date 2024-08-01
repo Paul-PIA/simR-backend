c9d01f48b1ca14dd29e905e5fb4db1e75316d82d
PIA-Noah <noah.balencourt@parisinfrastructureadvisory.com>
2024-08-01 16:36:42 +0200

    render.yaml 5

--- build.sh
+++ build.sh
@@ -11,5 +11,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 
 python manage.py makemigrations
+python manage.py makemigrations data
 python manage.py migrate
+python manage.py migrate data
 
