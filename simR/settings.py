f421d74defe386972fc1571a2292d53376c009f1
96587413 <le.zhang@parisinfrastructureadvisory.com>
2024-09-04 16:01:19 +0200

    deploy

--- simR/settings.py
+++ simR/settings.py
@@ -156,22 +156,22 @@ WSGI_APPLICATION = 'simR.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "NAME": "dj_test",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://user:t8IqN8PQQASla4eJrINTqSfRTJc9Ayni@dpg-cqllohrv2p9s73b2spg0-a/simr_database_pn3r',
-#         conn_max_age=600
-#     )  
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         "NAME": "dj_test",
+#         "USER": "postgres",
+#         "PASSWORD": "1234",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://simr_database_wvum_user:fTRA4HdZvvFub3H0YiIqqHBWKYTgIUHu@dpg-crc64523esus739725f0-a/simr_database_wvum',
+        conn_max_age=600
+    )  
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
